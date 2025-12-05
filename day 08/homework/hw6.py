75883be0c83a880a19c21cfffc8a1fcf5d85d262
ia201509 <iakvartskhava4@gmail.com>
2025-12-05 19:24:49 +0300

    1

---
+++ day 08/homework/hw6.py
@@ -0,0 +1,5 @@
+ #  6)ჩამოწერეთ რამდენი არგუმენტი შეიძლება გადაეცეს range()ფუნქცია და ახსენით რომელი რას აკეთებს
+ #  3 range(5), range(2, 7), range(10, 0, 2)
+ #  1 = sanamde mivides rom es ricxvi ar CaTvalos
+ #  2 = saidan daiwyos da sanamde mivibes
+ #  3 = saidan daiwyos , sanamde mivides da ramdeni nabibijit gadavides Semdeg ricxvze
