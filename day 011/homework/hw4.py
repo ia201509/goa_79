bdc97dd97c192c1b969974691787c35b3463b1fd
ia201509 <iakvartskhava4@gmail.com>
2026-01-04 16:00:30 +0400

    1

---
+++ day 011/homework/hw4.py
@@ -0,0 +1,19 @@
+#4)მოსწავლეს შეეკითხეთ მის მიერ მიღებული ქულა,თუ ქულა უდრის 100-ს მაშინ გამოუტანეთ "A Group),თუ იქნება 80-დან 99-მდე მაშინ გამოიტანეთ "B Group",თუ იქნება 40-დან 70-მდე მაშინ "C Group",დანარჩენ შემთხვევაში კი "D Group"
+score = int(input("შეიყვანე ქულა: "))
+
+if score == 100:
+    print("A Group")
+elif score >= 80:
+    print("B Group")
+elif score >= 40:
+    print("C Group")
+else:
+    print("D Group")
+if score == 100:
+    print("A Group")
+elif score >= 80:
+    print("B Group")
+elif score >= 40:
+    print("C Group")
+else:
+    print("D Group")
