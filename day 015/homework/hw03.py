d272fef4c6ccabbe1b3d05543d29f8c50212d233
ia201509 <iakvartskhava4@gmail.com>
2026-01-30 20:53:48 +0400

    1

---
+++ day 015/homework/hw03.py
@@ -0,0 +1,13 @@
+# 3️⃣
+# მოცემულია სია
+
+# animals = ['cat', 'dog', 'fox', 'wolf', 'bear', 'lion']
+
+# ამოიღე ელემენტები 'dog'-დან 'bear'-მდე (ჩათვლით).
+
+
+animals = ['cat', 'dog', 'fox', 'wolf', 'bear', 'lion']
+
+print(animals[1:4])
+
+print(animals[1:5])
---
+++ day 015/homework/hw05.py
@@ -0,0 +1,12 @@
+# 5️⃣
+# მოცემულია სია
+
+# days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+# ამოიღე ელემენტები 'tue'-დან 'fri'-მდე (ჩათვლით).
+
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+print(days[1:4])
+
+print(days[1:5])
