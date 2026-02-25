509e89da2bdac801953af4a1534f8df0ea07ea43
ia201509 <iakvartskhava4@gmail.com>
2026-02-25 18:37:23 +0400

    1

---
+++ day 019/homework/hw03.py
@@ -0,0 +1,5 @@
+# 3) მომხმარებელს შემოატანინეთ წინადადება და დაბეჭდეთ იგი პატარა ასოებით.
+
+user_input = input("შეიყვანე წინადადება: ")
+lowercase_string = user_input.lower()
+print(lowercase_string)
