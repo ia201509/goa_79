86c932fa98ee63ca027116824c07fc47ed1997e2
ia201509 <iakvartskhava4@gmail.com>
2026-07-12 14:40:00 +0400

    1

---
+++ day 036/homework/hw.py
@@ -0,0 +1,70 @@
+# https://www.codewars.com/kata/59dd3ccdded72fc78b000b25/train/python
+
+
+def whatday(num):
+    if num == 1:
+        return "Sunday"
+    elif num == 2:
+        return "Monday"
+    elif num == 3:
+        return "Tuesday"
+    elif num == 4:
+        return "Wednesday"
+    elif num == 5:
+        return "Thursday"
+    elif num == 6:
+        return "Friday"
+    elif num == 7:
+        return "Saturday"
+    else:
+        return "Wrong, please enter a number between 1 and 7"
+    
+
+
+
+
+
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+
+
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+
+
+
+
+
+# https://www.codewars.com/kata/523b623152af8a30c6000027/train/python
+
+
+
+def square(n):
+    return n * n
+
+
+
+
+# https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/python
+
+
+
+def reverse_seq(n):
+    return list(range(n, 0, -1))
+
+
+
+
+
+# https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
+
+
+
+
+def sum_array(a):
+    return sum(a)
