2b1010181f55201ce39bdd6b02e7aacb0d872141
ia201509 <iakvartskhava4@gmail.com>
2026-09-06 21:03:42 +0400

    1

---
+++ day 046/classwork/cw03.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5545f109004975ea66000086/train/python
+
+
+
+
+
+
+
+
+def is_divisible(n,x,y):
+    if n%x == 0 and n%y == 0:
+        return True
+    else:
+        return False
